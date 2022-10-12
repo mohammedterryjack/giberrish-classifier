917fc0ccbf8d34b9db0c5bd42303b4d9cdf367c4
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-10-12 15:50:23 +0100

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,13 @@ class GiberrishClassifier:
         self.threshold = data["threshold"]
         self.log_probability_matrix = data["probabilities"]
         self.recognised_characters = data["characters"]
+        
+    def get_gibberish(self, ) -> str:
+        return ''.join(filter(self.is_giberrish,text.split()))
 
+    def remove_gibberish(self, ) -> str:
+        return ''.join(filter(self.is_word,text.split()))
+        
     def is_giberrish(self, word:str) -> bool:
         return not self.is_word(word)
 
